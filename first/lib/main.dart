7f8e8352a8985901e8aa47534e2ab0de2068010e
Drew <drc920630@gmail.com>
2023-08-29 23:49:23 +0900

    flutter 타이머 제작

--- first/lib/main.dart
+++ first/lib/main.dart
@@ -1,76 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_made/screens/home_screen.dart';
 
 void main() {
   runApp(const App());
 }
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  List<int> numbers = [];
-
-  void onClicked() {
-    setState(() {
-      numbers.add(numbers.length);
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
         textTheme: const TextTheme(
-          titleLarge: TextStyle(color: Colors.red),
-        ),
-      ),
-      home: const Scaffold(
-        backgroundColor: Color.fromARGB(255, 255, 241, 199),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              MyLargeTitle(),
-            ],
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
           ),
         ),
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xFFE7626C),
+        ),
+        cardColor: const Color(0xFFF4EDDB),
       ),
-    );
-  }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  @override
-  void initState() {
-    super.initState();
-    print('hello!');
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    print('dispose!');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-          fontSize: 20, color: Theme.of(context).textTheme.titleLarge?.color),
+      home: const HomeScreen(),
     );
   }
 }
